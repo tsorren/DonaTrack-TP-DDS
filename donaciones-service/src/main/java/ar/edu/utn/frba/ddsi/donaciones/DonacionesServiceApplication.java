bca228206840534ae50c78e0ff2e7f856b8307f0
Tadeo <tsorrentino@frba.utn.edu.ar>
2026-04-26 17:53:59 -0300

    fix: mejoras de CI y faltaba ejecutar mvn spotless:apply

--- donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/DonacionesServiceApplication.java
+++ donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/DonacionesServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DonacionesServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DonacionesServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DonacionesServiceApplication.class, args);
+  }
 }
