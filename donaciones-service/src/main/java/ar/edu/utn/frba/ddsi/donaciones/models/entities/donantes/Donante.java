5ae2476bf85ff52c00b22b7dda4de3dde3cf8dfc
martinzaj <mzajdenberg@frba.utn.edu.ar>
2026-05-07 17:03:48 -0300

    feat: modelar donante y donaciones

---
+++ donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/donantes/Donante.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.frba.ddsi.donaciones.models.entities.donantes;
+
+import ar.edu.utn.frba.ddsi.common.Persona;
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.donaciones.Donacion;
+import lombok.Data;
+
+@Data
+public class Donante {
+    private Persona persona;
+    private java.util.List<Donacion> historialDonaciones = new java.util.ArrayList<>();
+
+    public void agregarDonacion(Donacion donacion) {
+        this.historialDonaciones.add(donacion);
+    }
+
+    public void quitarDonacion(Donacion donacion) {
+        this.historialDonaciones.remove(donacion);
+    }
+}
