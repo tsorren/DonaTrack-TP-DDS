ea03843b92e3dfe6ef880429413b4e0bd84d1cf8
MiriMiranda <mirossi@frba.utn.edu.ar>
2026-05-07 23:32:45 -0300

    tests de medios de comunicacion
    Co-authored-by: Bernardo Estigarribia <mestigarribia@frba.utn.edu.ar>

--- donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/persona/medioDeContacto/Telefono.java
+++ donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/persona/medioDeContacto/Telefono.java
@@ -1,6 +1,6 @@
 package ar.edu.utn.frba.ddsi.donaciones.models.entities.persona.medioDeContacto;
 
-public class Telefono {
+public class Telefono extends MedioDeContacto {
   private String caracteristica;
   private String codigoArea;
   private String numero;
---
+++ donaciones-service/src/test/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/persona/MediosDeComunicacionTests.java
@@ -0,0 +1,64 @@
+package ar.edu.utn.frba.ddsi.donaciones.models.entities.persona;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.persona.medioDeContacto.Correo;
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.persona.medioDeContacto.Telefono;
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.persona.medioDeContacto.WhatsApp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+  Humana persona;
+  Correo correo;
+  Telefono telefono;
+  WhatsApp whatsApp;
+
+  @BeforeEach
+  void setup() {
+    persona = new Humana();
+
+    correo = new Correo();
+    correo.setEsPredeterminado(false);
+
+    telefono = new Telefono();
+    telefono.setEsPredeterminado(false);
+
+    whatsApp = new WhatsApp();
+    whatsApp.setEsPredeterminado(false);
+  }
+
+  @Test
+  void agregarMedioDeContacto() {
+    persona.agregarMedioDeContacto(correo);
+    persona.agregarMedioDeContacto(telefono);
+
+    assertTrue(persona.getContactos().contains(correo));
+    assertTrue(persona.getContactos().contains(telefono));
+  }
+
+  @Test
+  void quitarMedioDeContacto() {
+    persona.agregarMedioDeContacto(telefono);
+    persona.agregarMedioDeContacto(whatsApp);
+
+    persona.quitarMedioDeContacto(telefono);
+
+    assertFalse(persona.getContactos().contains(telefono));
+    assertTrue(persona.getContactos().contains(whatsApp));
+  }
+
+  @Test
+  void alDefinirPredeterminadoElAnteriorDejaDeSerlo() {
+    persona.agregarMedioDeContacto(correo);
+    persona.agregarMedioDeContacto(whatsApp);
+
+    whatsApp.setEsPredeterminado(true);
+
+    persona.definirMedioDeContactoPredeterminado(correo);
+
+    assertFalse(whatsApp.getEsPredeterminado());
+    assertTrue(correo.getEsPredeterminado());
+  }
+}
