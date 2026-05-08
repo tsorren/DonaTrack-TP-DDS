7ae671501e02eb70e8db5b0f25307e08e494796b
MiriMiranda <mirossi@frba.utn.edu.ar>
2026-05-07 23:02:00 -0300

    arreglos de formato

--- donaciones-service/src/test/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/persona/JuridicaTest.java
+++ donaciones-service/src/test/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/persona/JuridicaTest.java
@@ -1,39 +1,39 @@
 package ar.edu.utn.frba.ddsi.donaciones.models.entities.persona;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class JuridicaTest {
 
-    Juridica juridica;
-    Humana representante1;
-    Humana representante2;
+  Juridica juridica;
+  Humana representante1;
+  Humana representante2;
 
-    @BeforeEach
-    void setup() {
-        juridica = new Juridica();
+  @BeforeEach
+  void setup() {
+    juridica = new Juridica();
 
-        representante1 = new Humana();
-        representante2 = new Humana();
-    }
+    representante1 = new Humana();
+    representante2 = new Humana();
+  }
 
-    @Test
-    void agregarRepresentante() {
-        juridica.agregarRepresentante(representante1);
+  @Test
+  void agregarRepresentante() {
+    juridica.agregarRepresentante(representante1);
 
-        assertTrue(juridica.getRepresentantes().contains(representante1));
-    }
+    assertTrue(juridica.getRepresentantes().contains(representante1));
+  }
 
-    @Test
-    void quitarRepresentante() {
-        juridica.agregarRepresentante(representante1);
-        juridica.agregarRepresentante(representante2);
+  @Test
+  void quitarRepresentante() {
+    juridica.agregarRepresentante(representante1);
+    juridica.agregarRepresentante(representante2);
 
-        juridica.quitarRepresentante(representante1);
+    juridica.quitarRepresentante(representante1);
 
-        assertFalse(juridica.getRepresentantes().contains(representante1));
-        assertTrue(juridica.getRepresentantes().contains(representante2));
-    }
+    assertFalse(juridica.getRepresentantes().contains(representante1));
+    assertTrue(juridica.getRepresentantes().contains(representante2));
+  }
 }
