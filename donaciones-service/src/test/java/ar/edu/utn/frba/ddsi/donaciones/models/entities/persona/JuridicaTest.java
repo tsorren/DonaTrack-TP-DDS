01d60398506e388980de9701b018972dc9134d72
MiriMiranda <mirossi@frba.utn.edu.ar>
2026-05-07 22:52:54 -0300

    tests de clase Juridica
    Co-authored-by: Bernardo Estigarribia <mestigarribia@frba.utn.edu.ar>

---
+++ donaciones-service/src/test/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/persona/JuridicaTest.java
@@ -0,0 +1,39 @@
+package ar.edu.utn.frba.ddsi.donaciones.models.entities.persona;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JuridicaTest {
+
+    Juridica juridica;
+    Humana representante1;
+    Humana representante2;
+
+    @BeforeEach
+    void setup() {
+        juridica = new Juridica();
+
+        representante1 = new Humana();
+        representante2 = new Humana();
+    }
+
+    @Test
+    void agregarRepresentante() {
+        juridica.agregarRepresentante(representante1);
+
+        assertTrue(juridica.getRepresentantes().contains(representante1));
+    }
+
+    @Test
+    void quitarRepresentante() {
+        juridica.agregarRepresentante(representante1);
+        juridica.agregarRepresentante(representante2);
+
+        juridica.quitarRepresentante(representante1);
+
+        assertFalse(juridica.getRepresentantes().contains(representante1));
+        assertTrue(juridica.getRepresentantes().contains(representante2));
+    }
+}
