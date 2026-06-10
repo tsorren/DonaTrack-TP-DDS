1a704fd2e831063898124a7ddf2ec882ddb1d23b
martinzaj <mzajdenberg@frba.utn.edu.ar>
2026-06-10 17:42:06 -0300

    refactor: distinguir errores de validacion y recursos inexistentes

--- common-lib/src/main/java/grupo5/common/errors/RecursoNoEncontradoException.java
+++ common-lib/src/main/java/grupo5/common/errors/RecursoNoEncontradoException.java
@@ -1,11 +1,11 @@
 package grupo5.common.errors;
 
 public class RecursoNoEncontradoException extends RuntimeException {
-  public RecursoNoEncontradoException(Object id) {
-    super("No se encontró el recurso con id: " + id);
+  public RecursoNoEncontradoException(String mensaje) {
+    super(mensaje);
   }
 
-  public RecursoNoEncontradoException(String message) {
-    super(message);
+  public RecursoNoEncontradoException(Object id) {
+    super("No se encontró el recurso con id: " + id);
   }
 }
