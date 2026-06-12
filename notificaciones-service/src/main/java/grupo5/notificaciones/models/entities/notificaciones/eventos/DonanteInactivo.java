db472e13191bcfe1c544d5546c2da5af65da0c49
Anushig04 <lerakian@frba.utn.edu.ar>
2026-06-12 19:10:49 -0300

    Crear evento de inactividad #367

---
+++ notificaciones-service/src/main/java/grupo5/notificaciones/models/entities/notificaciones/eventos/DonanteInactivo.java
@@ -0,0 +1,24 @@
+package grupo5.notificaciones.models.entities.notificaciones.eventos;
+
+import grupo5.notificaciones.models.entities.notificaciones.Notificacion;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DonanteInactivo extends EventoNotificable {
+    private Integer diasInactividad;
+
+    @Override
+    public List<Notificacion> generarNotificaciones() {
+        // Armamos un mensaje empático que fomente el regreso del donante
+        String mensaje = "¡Te extrañamos en DonaTrack! "
+                + "Hemos notado que han pasado " + diasInactividad + " días desde tu última actividad. "
+                + "Las entidades beneficiarias aún necesitan tu ayuda. ¡Vuelve a hacer la diferencia!";
+
+        Notificacion notificacion = new Notificacion(this.getPersona(), mensaje);
+
+        return List.of(notificacion);
+    }
+}
