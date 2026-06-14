8a1142e4bc4eac449cfa78ba9dcfbcd81e650bd4
suarezcamila <casuarez@frba.utn.edu,ar>
2026-06-14 07:12:27 -0300

    normalizador de texto y bienes + correccion de comparador de texto -  faltan test

--- donaciones-service/src/main/java/grupo5/donaciones/infraestructure/analizadores/NormalizadorBasicoTexto.java
+++ donaciones-service/src/main/java/grupo5/donaciones/infraestructure/analizadores/NormalizadorBasicoTexto.java
@@ -1,11 +1,28 @@
 package grupo5.donaciones.infraestructure.analizadores;
 
+import java.text.Normalizer;
+
 public class NormalizadorBasicoTexto implements Normalizador {
   @Override
   public String normalizar(String texto) {
     if (texto == null) {
       return null;
     }
-    return texto.trim().toLowerCase().replaceAll("[^a-z0-9\\s]", "");
+
+    String result = texto.trim().toLowerCase();
+
+    // separa letras base de acentos
+    result = Normalizer.normalize(result, Normalizer.Form.NFD);
+
+    // elimina acentos
+    result = result.replaceAll("\\p{M}", "");
+
+    // limpia símbolos pero conserva letras y números
+    result = result.replaceAll("[^a-z0-9\\s]", "");
+
+    // limpia espacios duplicados
+    result = result.replaceAll("\\s+", " ").trim();
+
+    return result;
   }
 }
