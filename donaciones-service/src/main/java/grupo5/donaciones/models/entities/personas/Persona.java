54266a581b46ae8f4504d578fb4b607c81b105b5
belennn24 <calencina@frba.utn.edu.ar>
2026-06-15 16:31:24 -0300

    agrego medios de contacto a Persona

--- donaciones-service/src/main/java/grupo5/donaciones/models/entities/personas/Persona.java
+++ donaciones-service/src/main/java/grupo5/donaciones/models/entities/personas/Persona.java
@@ -1,6 +1,9 @@
 package grupo5.donaciones.models.entities.personas;
 
 import grupo5.donaciones.models.privacidad.Anonimizable;
+import grupo5.notificaciones.models.entities.personas.MedioDeContacto;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +14,10 @@ public abstract class Persona implements Anonimizable {
   private TipoDocumento tipoDocumento;
   private String documento;
   private Direccion direccion;
+  private List<MedioDeContacto> mediosDeContacto;
 
   // fuerzo a que se cree con al menos 1 contacto valido
-  protected Persona() {}
+  protected Persona() {
+    this.mediosDeContacto = new ArrayList<>();
+  }
 }
