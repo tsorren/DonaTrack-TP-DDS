16573cdb4231ce4d5f1146ecd852515e02ab6f0e
Anushig04 <lerakian@frba.utn.edu.ar>
2026-06-17 12:09:33 -0300

    correccion de notificacionService

--- notificaciones-service/src/main/java/grupo5/notificaciones/services/NotificacionService.java
+++ notificaciones-service/src/main/java/grupo5/notificaciones/services/NotificacionService.java
@@ -2,6 +2,7 @@ package grupo5.notificaciones.services;
 
 import grupo5.notificaciones.models.entities.notificaciones.Notificacion;
 import grupo5.notificaciones.models.entities.notificaciones.eventos.EventoNotificable;
+import grupo5.notificaciones.models.ports.NotificacionSender;
 import grupo5.notificaciones.models.repositories.NotificacionRepository;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -9,16 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotificacionService {
   private final NotificacionRepository repository;
+  private final NotificacionSender sender;
 
-  public NotificacionService(NotificacionRepository repository) {
+  public NotificacionService(NotificacionRepository repository, NotificacionSender sender) {
     this.repository = repository;
+    this.sender = sender;
   }
 
   public void procesar(EventoNotificable evento) {
     List<Notificacion> notificaciones = evento.generarNotificaciones();
 
     for (Notificacion notificacion : notificaciones) {
-      notificacion.notificar(null);
+      notificacion.notificar(sender);
       repository.save(notificacion);
     }
   }
