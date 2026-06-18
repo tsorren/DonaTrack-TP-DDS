167fa62c52ded193a8d1d6cb9d4e2b4f991e02ee
Camila Belén Lencina <calencina@frba.utn.edu.ar>
2026-06-18 01:55:34 -0300

    Update donaciones-service/src/main/java/grupo5/donaciones/dto/donantes/DonanteInputDTO.java
    Co-authored-by: Tadeo Sorrentino <tsorrentino@frba.utn.edu.ar>

--- donaciones-service/src/main/java/grupo5/donaciones/dto/donantes/DonanteInputDTO.java
+++ donaciones-service/src/main/java/grupo5/donaciones/dto/donantes/DonanteInputDTO.java
@@ -4,9 +4,4 @@ import grupo5.donaciones.dto.personas.PersonaInputDTO;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
-public class DonanteInputDTO {
-  private PersonaInputDTO persona;
-  private String canalContacto;
-}
+public record DonacionInputDTO(UUID idPersona){}
