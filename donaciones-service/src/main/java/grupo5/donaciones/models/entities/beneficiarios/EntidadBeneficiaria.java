bbdcbaf52fb7e45115f4d68c1103b1c42e672e4e
belennn24 <calencina@frba.utn.edu.ar>
2026-06-26 19:55:15 -0300

    vuelvo a poner lista de necesidades en EntidadBenef

--- donaciones-service/src/main/java/grupo5/donaciones/models/entities/beneficiarios/EntidadBeneficiaria.java
+++ donaciones-service/src/main/java/grupo5/donaciones/models/entities/beneficiarios/EntidadBeneficiaria.java
@@ -3,8 +3,11 @@ package grupo5.donaciones.models.entities.beneficiarios;
 import grupo5.common.exceptions.ErrorCatalog;
 import grupo5.common.exceptions.ValidationException;
 import grupo5.common.repositories.AggregateRoot;
+import grupo5.donaciones.models.entities.necesidades.Necesidad;
 import grupo5.donaciones.models.entities.personas.Juridica;
 import grupo5.donaciones.models.privacidad.Anonimizable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +17,7 @@ import lombok.Setter;
 public class EntidadBeneficiaria implements Anonimizable, AggregateRoot {
   private final UUID id;
   private Juridica juridica;
+  private List<Necesidad> necesidades = new ArrayList<>();
 
   public EntidadBeneficiaria(Juridica juridica) {
     this.id = UUID.randomUUID();
@@ -21,6 +25,25 @@ public class EntidadBeneficiaria implements Anonimizable, AggregateRoot {
       throw new ValidationException(ErrorCatalog.ENTIDAD_BENEFICIARIA_SIN_PERSONA_JURIDICA);
     }
     this.juridica = juridica;
+  }
+
+  public void agregarNecesidad(Necesidad necesidad) {
+    if (necesidad == null) {
+      throw new ValidationException(ErrorCatalog.AGREGAR_NECESIDAD_NULA);
+    }
+    if (necesidades.contains(necesidad)) {
+      throw new ValidationException(ErrorCatalog.NECESIDAD_YA_REGISTRADA);
+    }
+    this.necesidades.add(necesidad);
+    necesidad.setEntidad(this);
+  }
+
+  public void quitarNecesidad(Necesidad necesidad) {
+    if (!this.necesidades.contains(necesidad)) {
+      throw new ValidationException(ErrorCatalog.NECESIDAD_NO_PERTENECE_A_ENTIDAD);
+    }
+    this.necesidades.remove(necesidad);
+    necesidad.setEntidad(null);
   }
 
   @Override
