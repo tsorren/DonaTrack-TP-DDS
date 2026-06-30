faeace7998de4ff1cbd9e9ec5c216acb48365b2d
belennn24 <calencina@frba.utn.edu.ar>
2026-06-30 00:24:51 -0300

    saco la referencia a Ruta de Camion

--- logistica-service/src/main/java/grupo5/logistica/models/entities/camiones/Camion.java
+++ logistica-service/src/main/java/grupo5/logistica/models/entities/camiones/Camion.java
@@ -2,7 +2,6 @@ package grupo5.logistica.models.entities.camiones;
 
 import grupo5.common.exceptions.ErrorCatalog;
 import grupo5.common.exceptions.ValidationException;
-import grupo5.logistica.models.entities.rutas.Ruta;
 import java.util.UUID;
 import lombok.Getter;
 
@@ -14,7 +13,6 @@ public class Camion {
   private final Float altura;
   private final Float capacidadKG;
   private EstadoCamion estado;
-  private Ruta rutaActual;
 
   public Camion(String patente, Float capacidadVolumen, Float capacidadKG, Float altura) {
     this.id = UUID.randomUUID();
@@ -23,22 +21,6 @@ public class Camion {
     this.capacidadKG = capacidadKG;
     this.altura = altura;
     this.estado = EstadoCamion.DISPONIBLE;
-  }
-
-  public void asignarRuta(Ruta ruta) {
-    if (this.estado != EstadoCamion.DISPONIBLE) {
-      throw new ValidationException(ErrorCatalog.ESTADO_CAMION_TRANSICION_INVALIDA);
-    }
-    this.estado = EstadoCamion.EN_RUTA;
-    this.rutaActual = ruta;
-  }
-
-  public void liberarRuta() {
-    if (this.estado != EstadoCamion.EN_RUTA) {
-      throw new ValidationException(ErrorCatalog.ESTADO_CAMION_TRANSICION_INVALIDA);
-    }
-    this.estado = EstadoCamion.DISPONIBLE;
-    this.rutaActual = null;
   }
 
   public void mandarAMantenimiento() {
