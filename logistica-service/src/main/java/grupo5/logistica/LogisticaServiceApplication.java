46f2b2d0100ece3414ce25a2787d006fdfe8b013
Anushig04 <lerakian@frba.utn.edu.ar>
2026-07-02 19:59:50 -0300

    delegue la logica del planificador al service

--- logistica-service/src/main/java/grupo5/logistica/LogisticaServiceApplication.java
+++ logistica-service/src/main/java/grupo5/logistica/LogisticaServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(scanBasePackages = "grupo5")
 @EnableScheduling
 @EnableFeignClients(basePackages = "grupo5.logistica.infrastructure.clients")
-public class LogisticaServiceApplication {
+public class LLogisticaServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(LogisticaServiceApplication.class, args);
   }
