c86f0776c183243745f5e76ec62473f2b12663d6
Tadeo <tsorrentino@frba.utn.edu.ar>
2026-07-14 00:18:14 -0300

    Fix inactividad incentivos

--- incentivos-service/src/main/java/grupo5/incentivos/models/entities/inactividad/InactividadDonaciones.java
+++ incentivos-service/src/main/java/grupo5/incentivos/models/entities/inactividad/InactividadDonaciones.java
@@ -1,24 +1,30 @@
 package grupo5.incentivos.models.entities.inactividad;
 
 import grupo5.incentivos.models.entities.donante.DonanteIncentivos;
+import java.time.Clock;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.List;
 
 public class InactividadDonaciones extends CriterioInactividad {
 
   private final int diasSinDonar;
+  private final Clock clock;
 
   public InactividadDonaciones(int diasSinDonar) {
+    this(diasSinDonar, Clock.systemDefaultZone());
+  }
+
+  public InactividadDonaciones(int diasSinDonar, Clock clock) {
     if (diasSinDonar <= 0) {
       throw new IllegalArgumentException("Los días de inactividad deben ser mayores a cero");
     }
     this.diasSinDonar = diasSinDonar;
+    this.clock = clock;
   }
 
   @Override
   public List<DonanteIncentivos> detectarInactivos(List<DonanteIncentivos> donantes) {
-    LocalDate umbral = LocalDate.now(ZoneId.systemDefault()).minusDays(diasSinDonar);
+    LocalDate umbral = LocalDate.now(clock).minusDays(diasSinDonar);
     return donantes.stream().filter(d -> esInactivo(d, umbral)).toList();
   }
 
