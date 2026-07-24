0e72467155c964260a3a1eedd54c4489487024d5
Tadeo <tsorrentino@frba.utn.edu.ar>
2026-07-24 00:55:03 -0300

    Logistica - Diseño de Tests de Integracion

---
+++ integration-tests/src/test/java/grupo5/tests/integration/LogisticaIT.java
@@ -0,0 +1,26 @@
+package grupo5.tests.integration;
+
+import grupo5.tests.BaseIT;
+
+public class LogisticaIT extends BaseIT {
+
+    /*
+    TEST DE INTEGRACION 1:
+
+    1) En Servicio de Donaciones
+    - Crear persona 1
+    - Crear donante asociado a persona 1
+    - Crear donacion
+    - Crear persona juridica 2
+    - Crear entidad beneficiaria asociado a persona 2
+    - Crear necesidad
+    - Ejecutar algoritmo de asignacion
+
+    2) En Servicio de Logistica
+    - Crear camion
+    - Crear chofer
+    - Verificar que se creo la entrega
+    - Ejecutar generador de rutas
+    - Transicionar estados de entrega verificando impacto en Servicio de Donaciones y Servicio de Notificaciones (todos los estados)
+     */
+}
