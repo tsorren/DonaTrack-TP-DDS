32ddbe61127a30afaa7cbb1e8c517db8f918d3ff
belennn24 <calencina@frba.utn.edu.ar>
2026-08-24 21:03:52 -0300

    cambio callbacks por /resultados

--- logistica-service/src/main/java/grupo5/logistica/controllers/impl/PlanificacionController.java
+++ logistica-service/src/main/java/grupo5/logistica/controllers/impl/PlanificacionController.java
@@ -28,7 +28,7 @@ public class PlanificacionController implements IPlanificacionController {
   }
 
   @Override
-  @PostMapping("/callbacks/rutas")
+  @PostMapping("/resultados")
   public ResponseEntity<SolicitudPlanificacionResponseDTO> procesarCallback(
       @RequestBody CallbackPlanificacionRequestDTO dto) {
     return ResponseEntity.ok(planificacionService.procesarCallback(dto));
