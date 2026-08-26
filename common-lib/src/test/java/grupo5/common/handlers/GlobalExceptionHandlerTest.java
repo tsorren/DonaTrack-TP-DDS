a3504627c8b48413378776c030db89c2289780d6
sofiadeane <sdeane@frba.utn.edu.ar>
2026-08-25 23:00:14 -0300

    me olvide un import

--- common-lib/src/test/java/grupo5/common/handlers/GlobalExceptionHandlerTest.java
+++ common-lib/src/test/java/grupo5/common/handlers/GlobalExceptionHandlerTest.java
@@ -1,6 +1,7 @@
 package grupo5.common.handlers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
