a5a05968efff23228330f4fb2a98899e477efa96
Tadeo Sorrentino <tsorrentino@frba.utn.edu.ar>
2026-08-27 20:30:40 -0300

    Work in Progress

--- logistica-service/src/main/java/grupo5/logistica/controllers/impl/RutasController.java
+++ logistica-service/src/main/java/grupo5/logistica/controllers/impl/RutasController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
 @RestController
 @RequestMapping("/api/rutas")
 public class RutasController implements IRutasController {
