f64d4107cd2543ace54679bc0162d149ae7f7b4c
Anushig04 <lerakian@frba.utn.edu.ar>
2026-09-02 12:45:14 -0300

    agregar import faltante

--- incentivos-service/src/test/java/grupo5/incentivos/IncentivosServiceApplicationTest.java
+++ incentivos-service/src/test/java/grupo5/incentivos/IncentivosServiceApplicationTest.java
@@ -3,10 +3,13 @@ package grupo5.incentivos;
 import grupo5.incentivos.infrastructure.clients.NotificacionesFeignClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class IncentivosServiceApplicationTest {
-  @MockitoBean private NotificacionesFeignClient notificacionesFeignClient;
+
+  @MockitoBean
+  private NotificacionesFeignClient notificacionesFeignClient;
 
   @Test
   void contextLoads() {}
