6424294590f2a00b7b729c8cf093f61f1556a905
Anushig04 <lerakian@frba.utn.edu.ar>
2026-09-02 12:41:42 -0300

    test: mockear clientes Feign en tests de contexto de donaciones e incentivos

--- incentivos-service/src/test/java/grupo5/incentivos/IncentivosServiceApplicationTest.java
+++ incentivos-service/src/test/java/grupo5/incentivos/IncentivosServiceApplicationTest.java
@@ -1,10 +1,13 @@
 package grupo5.incentivos;
 
+import grupo5.incentivos.infrastructure.clients.NotificacionesFeignClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class IncentivosServiceApplicationTest {
+  @MockitoBean private NotificacionesFeignClient notificacionesFeignClient;
+
   @Test
   void contextLoads() {}
 }
