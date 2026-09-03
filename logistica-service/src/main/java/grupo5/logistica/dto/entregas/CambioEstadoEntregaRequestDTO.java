30e89ad089625a0410742cfe565b1cccc5a0beb7
belennn24 <calencina@frba.utn.edu.ar>
2026-09-03 16:42:19 -0300

    bean validation en request DTO de entrega

--- logistica-service/src/main/java/grupo5/logistica/dto/entregas/CambioEstadoEntregaRequestDTO.java
+++ logistica-service/src/main/java/grupo5/logistica/dto/entregas/CambioEstadoEntregaRequestDTO.java
@@ -1,10 +1,12 @@
 package grupo5.logistica.dto.entregas;
 
 import grupo5.logistica.models.entities.entregas.EstadoEntrega;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CambioEstadoEntregaRequestDTO(
-    EstadoEntrega estado,
-    String actor,
+    @NotNull(message = "El estado es obligatorio") EstadoEntrega estado,
+    @NotBlank(message = "El actor es obligatorio") String actor,
     String justificacion, // Utilizado cuando el estado es NO_RECIBIDA
     Boolean replanificable // Utilizado cuando el estado es NO_RECIBIDA
     ) {}
